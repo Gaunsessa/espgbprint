0896c220a4aec7a9a44276946a632f033683e0c4
Gus <gus@ef.je>
2022-12-27 01:25:54 +1100

    Added Gameboy Color Palette Swaps

--- src/net.c
+++ src/net.c
@@ -9,8 +9,30 @@
 const char *INDEX_SOURCE = str(
     <canvas id='img' width='160' height='144'></canvas>
     <br>
-    <button onclick='read_img()'>READ</button>
+    <button onclick='read_img(null)'>READ</button>
+    <button onclick='save_img()'>SAVE</button>
+    <br>
+    <input type='checkbox' id='gbc'>
+    <p>GBC:</p>
+    <select id='dpad'>
+        <option value='N'>NONE</option>
+        <option value='U'>UP</option>
+        <option value='D'>DOWN</option>
+        <option value='L'>LEFT</option>
+        <option value='R'>RIGHT</option>
+    </select>
+    <select id='buttons'>
+        <option value='N'>NONE</option>
+        <option value='B'>B</option>
+        <option value='A'>A</option>
+    </select>
     <style>
+        * {
+            font-family: Arial, sans-serif;
+
+            margin: 0.5vh;
+        }
+
         body {
             text-align: center;
         }
@@ -25,15 +47,44 @@ const char *INDEX_SOURCE = str(
         }
 
         button {
-            width: 75vmin;
+            width: 37.5vmin;
             height: 5vw;
 
             font-size: 2vw;
+
+            margin-bottom: 2vh;
+        }
+
+        p {
+            display: inline;
         }
     </style>
     <script defer>
-        const cavnas = document.querySelector('#img');
+        const cavnas  = document.querySelector('#img');
+        const gbcen   = document.querySelector('#gbc');
+        const dpad    = document.querySelector('#dpad');
+        const buttons = document.querySelector('#buttons');
+
         const ctx = cavnas.getContext('2d');
+
+        const palettes = {
+            '  ': [[255, 255, 255], [166, 167, 166], [81, 81, 81]   , [0, 0, 0]],
+            'NN': [[255, 255, 255], [124, 255, 44] , [0, 99, 199]   , [0, 0, 0]],
+            'UN': [[255, 255, 255], [255, 175, 99] , [132, 45, 0]   , [0, 0, 0]],
+            'UA': [[255, 255, 255], [255, 134, 133], [149, 55, 55]  , [0, 0, 0]],
+            'UB': [[255, 232, 198], [207, 157, 134], [133, 107, 36] , [91, 45, 2]],
+            'LN': [[255, 255, 255], [101, 166, 156], [0, 0, 254]    , [0, 0, 0]],
+            'LA': [[255, 255, 255], [140, 141, 223], [82, 81, 141]  , [0, 0, 0]],
+            'LB': [[255, 255, 255], [166, 167, 166], [81, 81, 81]   , [0, 0, 0]],
+            'DN': [[255, 255, 166], [254, 149, 149], [148, 149, 254], [0, 0, 0]],
+            'DA': [[255, 255, 255], [255, 255, 0]  , [254, 0, 0]    , [0, 0, 0]],
+            'DB': [[255, 255, 255], [255, 255, 0]  , [126, 71, 0]   , [0, 0, 0]],
+            'RN': [[255, 255, 255], [80, 255, 0]   , [255, 64, 0]   , [0, 0, 0]],
+            'RA': [[255, 255, 255], [124, 255, 44] , [0, 99, 199]   , [0, 0, 0]],
+            'RB': [[0, 0, 0]      , [0, 133, 135]  , [255, 223, 0]  , [255, 255, 255]]
+        };
+
+        let prev_data = null;
 
         function set_pixel(img, x, y, color) {
             const pos = x * 4 + y * 4 * 160;
@@ -44,8 +95,16 @@ const char *INDEX_SOURCE = str(
             img.data[pos + 3] = 255;
         }
 
-        async function read_img() {
-            let data = new Uint8Array(await (await (await fetch('/img')).blob()).arrayBuffer());
+        async function read_img(inp_data) {
+            let data = inp_data == null 
+                ? new Uint8Array(await (await (await fetch('/img')).blob()).arrayBuffer()) 
+                : inp_data;
+
+            prev_data = data;
+
+            const palette = palettes[
+                dpad.value == 'N' && buttons.value != 'N' ? 'NN' : (gbcen.checked ? dpad.value + buttons.value : '  ')
+            ];
 
             let inp = [];
 
@@ -55,7 +114,7 @@ const char *INDEX_SOURCE = str(
                         ((data[i] & (1 << bit)) >> bit) + (((data[i + 1] & (1 << bit)) >> bit) << 1)
                     );
 
-            inp = inp.map((x) => { return [[255, 255, 255], [191, 191, 191], [127, 127, 127], [63, 63, 63]][x]; });
+            inp = inp.map((x) => { return palette[x]; });
 
             let out = ctx.createImageData(160, 144);
 
@@ -72,6 +131,21 @@ const char *INDEX_SOURCE = str(
 
             ctx.putImageData(out, 0, 0);   
         }
+
+        function save_img() {
+            let a = document.createElement('a');
+
+            a.download = 'gb.png';
+            a.href     = cavnas.toDataURL();
+
+            a.click();
+
+            a.remove();
+        }
+
+        gbcen.addEventListener('change', () => { if (prev_data != null) read_img(prev_data); });
+        dpad.addEventListener('change', () => { if (prev_data != null) read_img(prev_data); });
+        buttons.addEventListener('change', () => { if (prev_data != null) read_img(prev_data); });
     </script>
 );
 
@@ -137,7 +211,7 @@ void ICACHE_FLASH_ATTR web_recv(void *arg, char *data, uint16_t len) {
 
     data += 4;
 
-    char buffer[2000];
+    char buffer[5000];
 
     if (!memcmp("/ ", data, 2)) {
         os_sprintf(
